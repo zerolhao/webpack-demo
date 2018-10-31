6670ac8975012634889606bd58a7ef1741ffb674
zerolhao <zerolhao@gmail.com>
2018-10-31 18:14:12 +0800

    资源管理，加载字体

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,14 @@ module.exports = {
   },
   module: {
     rules: [
-      {
+      { // 处理 .css 文件
         test: /\.css$/,
         use: [
           'style-loader',
           'css-loader'
         ]
       },
-      {
+      { // 处理 图片
         test: /\.(png|jpg|gif)$/,
         use: [
           {
@@ -24,6 +24,12 @@ module.exports = {
               limit: 8192
             }
           }
+        ]
+      },
+      { // 处理 字体
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
         ]
       }
     ]
