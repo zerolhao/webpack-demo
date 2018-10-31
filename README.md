62a05777c159d74a3aa5f799e10cd29844ea8787
zerolhao <zerolhao@gmail.com>
2018-10-31 16:15:07 +0800

    资源管理，加载图片，使用file-loader

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. 安装和基础构建
 2. 资源管理([配置](https://webpack.docschina.org/guides/asset-management))
     - 加载css `npm install --save-dev style-loader css-loader`
+    - 加载图片'npm install --save-dev file-loader'
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 import './style.css';
+import IconHome from './icon-home.png';
+import IconEye from './icon-eye.png';
 
 function component() {
   var element = document.createElement('div')
@@ -7,6 +9,12 @@ function component() {
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
   element.classList.add('hello')
+
+  // 将图像添加到我们现有的 div
+  var iconHome = new Image()
+  iconHome.src = IconHome
+
+  element.appendChild(iconHome)
 
   return element
 }
